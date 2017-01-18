ef30d899b3670f16f1bb3bcb38f05a601c66e2b9
ilex <ilexcorp@ukr.net>
2017-01-19 00:30:06 +0200

    Change global aiohttp_auth setup function.
    Replace setting up acl middleware with setting up autz middleware.

--- aiohttp_auth/__init__.py
+++ aiohttp_auth/__init__.py
@@ -1,21 +1,18 @@
 from .auth import auth_middleware
+from .autz import autz_middleware
 from .acl import acl_middleware
-from . import auth, acl
+from . import auth, acl, autz
 
 
-def setup(app, auth_policy, acl_groups_callback):
-    """Setup auth and acl middleware in aiohttp fashion.
+def setup(app, auth_policy, autz_policy):
+    """Setup auth and autz middleware in aiohttp fashion.
 
     Args:
         app: aiohttp Application object.
         auth_policy: An authentication policy with a base class of
             AbstractAuthentication.
-        acl_groups_callback: This is a callable which takes a user_id (as
-            returned from the auth.get_auth function), and expects a sequence
-            of permitted ACL groups to be returned. This can be a empty tuple
-            to represent no explicit permissions, or None to explicitly forbid
-            this particular user_id. Note that the user_id passed may be None
-            if no authenticated user exists.
+        autz_policy: An authorization policy with a base class of
+            AbstractAutzPolicy
     """
     auth.setup(app, auth_policy)
-    acl.setup(app, acl_groups_callback)
+    autz.setup(app, autz_policy)
--- pytests/test_aiohttp_auth.py
+++ pytests/test_aiohttp_auth.py
@@ -1,7 +1,8 @@
 import aiohttp_auth
 import aiohttp_session
 from aiohttp import web
-from aiohttp_auth import acl, auth
+from aiohttp_auth import autz, auth
+from aiohttp_auth.autz.policy import acl
 
 
 async def test_aiohttp_auth_middleware_setup(loop):
@@ -11,15 +12,19 @@ async def test_aiohttp_auth_middleware_setup(loop):
     storage = aiohttp_session.SimpleCookieStorage()
     aiohttp_session.setup(app, storage)
 
-    policy = auth.SessionTktAuthentication(secret, 15, cookie_name='auth')
+    auth_policy = auth.SessionTktAuthentication(secret, 15,
+                                                cookie_name='auth')
 
-    async def acl_groups_callback(user_id):
-        pass
+    class ACLAutzPolicy(acl.AbsractACLAutzPolicy):
+        async def acl_groups(self, user_identity):
+            return None  # pragma: no cover
 
-    aiohttp_auth.setup(app, policy, acl_groups_callback)
+    autz_policy = ACLAutzPolicy()
 
-    middleware = auth.auth_middleware(policy)
+    aiohttp_auth.setup(app, auth_policy, autz_policy)
+
+    middleware = auth.auth_middleware(auth_policy)
     assert app.middlewares[-2].__name__ == middleware.__name__
 
-    middleware = acl.acl_middleware(acl_groups_callback)
+    middleware = autz.autz_middleware(autz_policy)
     assert app.middlewares[-1].__name__ == middleware.__name__
